78b8454a5ac056e6cb62a0d8e3322a58e3e6c49a
Evan You <yyx990803@gmail.com>
2020-05-19 16:24:06 -0400

    update react template

--- template-react/src/App.jsx
+++ template-react/src/App.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
         <p>
-          Edit <code>App.jsx</code> and save to reload.
+          <button onClick={() => setCount(count + 1)}>count is: {count}</button>
+        </p>
+        <p>
+          Edit <code>App.jsx</code> and save to test HMR updates.
         </p>
         <a
           className="App-link"

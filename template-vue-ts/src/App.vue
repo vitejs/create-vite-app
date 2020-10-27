cd4ff7a93deeb6bdaef6a42111a6275c19e3cf27
Evan You <yyx990803@gmail.com>
2020-10-26 20:04:12 -0400

    updates

--- template-vue-ts/src/App.vue
+++ template-vue-ts/src/App.vue
@@ -4,13 +4,12 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
 
-export default defineComponent({
+export default {
   name: 'App',
   components: {
-    HelloWorld,
-  },
-})
+    HelloWorld
+  }
+}
 </script>
--- template-vue-ts/src/components/HelloWorld.vue
+++ template-vue-ts/src/components/HelloWorld.vue
@@ -1,5 +1,21 @@
 <template>
   <h1>{{ msg }}</h1>
+  <p>
+    Recommended setup:
+    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
+    +
+    <a
+      href="https://marketplace.visualstudio.com/items?itemName=octref.vetur"
+      target="_blank"
+      >Vetur</a
+    >
+  </p>
+  <p>
+    Make sure to use workspace version of TypeScript to get improved support via
+    <a href="https://github.com/znck/vue-developer-experience" target="_blank"
+      >@vuedx</a
+    >.
+  </p>
   <button @click="count++">count is: {{ count }}</button>
   <p>
     Edit <code>components/HelloWorld.vue</code> to test hot module replacement.
@@ -7,19 +23,19 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue'
+import { ref } from 'vue'
 
-export default defineComponent({
+export default {
   name: 'HelloWorld',
   props: {
     msg: {
       type: String,
-      required: true,
-    },
+      required: true
+    }
   },
   setup: () => {
     const count = ref(0)
     return { count }
-  },
-})
+  }
+}
 </script>
